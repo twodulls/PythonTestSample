da88881eb424c88d0b1799eb1a42b8218d698df4
twodulls <twodulls@naver.com>
2019-08-20 17:01:13 +0900

    Update webcrawlerTest.py
    url 변경

--- webcrawler/webcrawlerTest.py
+++ webcrawler/webcrawlerTest.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = 'http://repos.tmonc.net/cgit/cgit.cgi/www.git/plain/SG/config/www/rpc.qa.php'
+url = 'http://xxx.test.com' ///URL 변경해야 한다.
 response = urlopen(url)
 plain_text = response.read()
 soup = BeautifulSoup(plain_text, 'html.parser')
